219738ba09dc7959e5872a5d5f9d1242f9e22643
ryan <laslomcd@gmail.com>
2024-07-22 15:49:05 -0400

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,70 @@
+const btn = document.getElementById("menu-btn");
+const overlay = document.getElementById("overlay");
+const menu = document.getElementById("mobile-menu");
+const counters = document.querySelectorAll(".counter");
+let scrollStarted = false;
+let lastScrollPos = 0;
+
+const header = document.getElementById("main-header");
+
+btn.addEventListener("click", navToggle);
+document.addEventListener("scroll", () => {
+	scrollPage(), hideNav();
+});
+
+function hideNav() {
+	const scrollPos = window.scrollY;
+
+	if (scrollPos > lastScrollPos) {
+		header.style.opacity = 0;
+		btn.style.opacity = 0;
+	} else {
+		header.style.opacity = 1;
+		btn.style.opacity = 1;
+	}
+
+	lastScrollPos = scrollPos;
+}
+
+function navToggle() {
+	btn.classList.toggle("open");
+	overlay.classList.toggle("overlay-show");
+	document.body.classList.toggle("stop-scrolling");
+	menu.classList.toggle("show-menu");
+}
+
+function scrollPage() {
+	const scrollPos = window.scrollY;
+	if (scrollPos > 100 && !scrollStarted) {
+		countUp();
+		scrollStarted = true;
+	} else if (scrollPos < 100 && scrollStarted) {
+		reset();
+		scrollStarted = false;
+	}
+}
+
+function countUp() {
+	counters.forEach((counter) => {
+		counter.innerText = "0";
+
+		const updateCounter = () => {
+			const target = +counter.getAttribute("data-target");
+			const c = +counter.innerText;
+			const increment = target / 100;
+
+			if (c < target) {
+				counter.innerText = `${Math.ceil(c + increment)}`;
+				setTimeout(updateCounter, 50);
+			} else {
+				counter.innerText = target;
+			}
+		};
+
+		updateCounter();
+	});
+}
+
+function reset() {
+	counters.forEach((counter) => (counter.innerHTML = "0"));
+}
